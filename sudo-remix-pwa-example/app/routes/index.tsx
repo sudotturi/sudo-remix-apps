455140678818a1d70044c359db72bc87bc54f512
RAHUL C H <46349786+sudotturi@users.noreply.github.com>
2022-12-30 11:21:36 +0000

    dev

---
+++ sudo-remix-pwa-example/app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div >
+      <a target="_blank" href="https://www.sudofolks.com" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", display:"flex", justifyContent:"center", alignItems:"center", height:"100vh" , textDecoration:"none"}}><h1>Sudo Folks</h1></a>
+    </div>
+  );
+}
