ab480e51f636a01188a5a096e0359ef5b02a3630
RAHUL C H <46349786+sudotturi@users.noreply.github.com>
2022-12-30 11:26:47 +0000

    dev

--- sudo-remix-pwa-example/app/root.tsx
+++ sudo-remix-pwa-example/app/root.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useLocation, useMatches } from '@remix-run/react';
 import type { MetaFunction } from "@remix-run/node";
 import {
   Links,
@@ -7,26 +9,61 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
+let isMount = true;
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
-
 export default function App() {
+  let location = useLocation();
+  let matches = useMatches();
+
+  React.useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
+
   return (
     <html lang="en">
+       
       <head>
-        <Meta />
-        <Links />
-      </head>
+      <link rel="manifest" href="/resources/manifest.webmanifest" />
+        <Meta /> <Links /> 
+      </head> 
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+         
+        <Outlet /> <ScrollRestoration /> <Scripts /> <LiveReload /> 
+      </body> 
     </html>
   );
 }
